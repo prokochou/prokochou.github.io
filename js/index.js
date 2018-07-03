f9e26a78fd70105555989751babcefe5a94b33d0
proko.chou <proko.chou@cloudmosa.com>
2018-07-03 21:02:07 +0800

    Adjust the size of myPhoto for mobile phone's screen.

--- js/index.js
+++ js/index.js
@@ -11,11 +11,20 @@ $(function(){
 	var thisID=0;
 
 	window.setInterval(function(){
+		//get screen width
+		var width = $(window).width(); 
+
 		if(thisID==0){
 			$('.about-photo').css('width','250px');
 			$('.about-photo').attr('src', imgArray[thisID]);
 		}else{
-			$('.about-photo').css('width','500px');
+			console.log(width);
+			if (width>375){
+				$('.about-photo').css('width','500px');
+			}else{
+				$('.about-photo').css('width','300px');
+			}
+			console.log(thisID)
 			$('.about-photo').attr('src', imgArray[thisID]);
 		}
 
